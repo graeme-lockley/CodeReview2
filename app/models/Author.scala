29b6f46a011a0c51897a6d7518982a835aba001d
Graeme Lockley <graemel@no9.co.za>
2014-08-09 17:14:14 +0200

    Reformatted

--- app/models/Author.scala
+++ app/models/Author.scala
@@ -1,18 +1,18 @@
 package models
 
-import org.squeryl.PrimitiveTypeMode._
 import adaptors.DBAuthor
+import org.squeryl.PrimitiveTypeMode._
 
 class Author(val id: AuthorID, val name: String, val emailAddress: Option[EmailAddress], val isAdmin: Boolean) {
-    override def toString: String = (id, name, emailAddress).toString()
+  override def toString: String = (id, name, emailAddress).toString()
 }
 
 object Author {
-    def apply(id: AuthorID, name: String, emailAddress: Option[EmailAddress], isAdmin: Boolean) = new Author(id, name, emailAddress, isAdmin)
+  def apply(id: AuthorID, name: String, emailAddress: Option[EmailAddress], isAdmin: Boolean) = new Author(id, name, emailAddress, isAdmin)
 
-	def all(): Traversable[Author] = inTransaction {
-	 	DBAuthor.all().map(x => convertDBAuthorToAuthor(x))
-	}
+  def all(): Traversable[Author] = inTransaction {
+    DBAuthor.all().map(x => convertDBAuthorToAuthor(x))
+  }
 
-	private def convertDBAuthorToAuthor(dbAuthor: DBAuthor): Author = Author(dbAuthor.id, dbAuthor.name, dbAuthor.emailAddress, dbAuthor.isAdmin)
+  private def convertDBAuthorToAuthor(dbAuthor: DBAuthor): Author = Author(dbAuthor.id, dbAuthor.name, dbAuthor.emailAddress, dbAuthor.isAdmin)
 }
