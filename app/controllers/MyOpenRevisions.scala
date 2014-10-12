de9f98e4e1467811e6daa7902a693bb0a4818569
Graeme Lockley <graemel@no9.co.za>
2014-10-12 17:27:17 +0200

    Create a layer busting query to provide a list of all the open all of the open revisions that are associated with a passed author.

---
+++ app/controllers/MyOpenRevisions.scala
@@ -0,0 +1,29 @@
+package controllers
+
+import models.Author
+import org.squeryl.PrimitiveTypeMode._
+import ports.Library
+
+class MyOpenRevisions(val me: Author) {
+  def items() = inTransaction {
+    dbItems().map(r => models.Revision.dbToModel(models.Repo.get(r.repoID), r))
+  }
+
+  private def dbItems() =
+    from(Library.revisions, Library.revisionEntries, Library.revisionEntryFeedback, Library.repoAuthors)((r, re, ref, ra) =>
+      where(r.id === re.revisionID and re.id === ref.revisionEntryID and
+        ref.parentID.isNull and ref.status.id === 1 and
+        r.repoAuthorID === ra.id and
+        ra.id === me.id).select(r)).distinct
+}
+
+object MyOpenRevisions {
+  def main(args: Array[String]) {
+    val app = new play.core.StaticApplication(new java.io.File("."))
+    val session = org.squeryl.Session.create(play.api.db.DB.getConnection()(app.application), new org.squeryl.adapters.H2Adapter)
+
+    for (x <- new MyOpenRevisions(models.Author.get(1)).items()) {
+      println(x)
+    }
+  }
+}
